e7890afde3614527b131a6f92df3a49d8afeafb7
Ameya <ameya@ameya-rvz03.lan>
2025-02-24 00:08:32 +1300

    This function allows reading stdout from commands like grep so that potentially the file on disk can be large but simple operations like filter can be outsourced before loading the data in RAM

---
+++ stdout_to_pd.py
@@ -0,0 +1,21 @@
+import subprocess as sp
+import pandas as pd
+import os
+
+def read_stdout(cmd, reader, sep, col_names):
+    tt = sp.run(cmd, stdout=sp.PIPE, shell=True, universal_newlines=True)
+    with open('out.txt', 'w') as output:
+        output.write(tt.stdout)
+    match reader:
+        case 'table':
+            p = pd.read_table(
+                filepath_or_buffer='out.txt', delimiter='\s+', 
+                names = col_names        
+            )
+        case 'csv':
+            p = pd.read_csv(
+                filepath_or_buffer='out.txt', sep=sep, names=col_names                
+            )
+    os.remove('out.txt')
+    return p
+
