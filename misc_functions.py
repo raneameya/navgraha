e9fb349957aae9ecc892a7d28219e3f90bd4e10f
Your Name <you@example.com>
2025-03-01 21:05:53 +1300

    Added house and sign computations

--- misc_functions.py
+++ misc_functions.py
@@ -62,6 +62,14 @@ def swetest(sweedir, birth_args):
     p=add_ketu(p)    
     # Reorder rows sensibly
     p=reorder_swetest_rows(p)
+    # House calculation
+    p=add_house(p)
+    return p
+
+def add_house(p):
+    p['Sign']=p['Lon'].apply(lambda x: int(divmod(x, 30)[0]+1))
+    lagna_rashi=p.loc[p['Graha']=='Lagna', 'Sign']
+    p['House']=p['Sign'].apply(lambda x: ((x+(12-lagna_rashi))%12)+1)
     return p
 
 def reorder_swetest_rows(p):
