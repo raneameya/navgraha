049ee6a6165d76617604c0c577f4ff183ca86e44
Your Name <you@example.com>
2025-03-01 21:05:19 +1300

    Simplified add_ketu and adde more comments

--- misc_functions.py
+++ misc_functions.py
@@ -2,7 +2,6 @@ from datetime import datetime
 import pytz, re
 import stdout_to_pd as sp
 import pandas as pd
-import numpy as np
 
 def create_date_from_txt(yr, mo, da, hr, mi, se, tz):
     ## Somewhat complex process of timezone conversion.
@@ -53,15 +52,15 @@ def swetest(sweedir, birth_args):
         ), 
         reader='table', sep='\s+', col_names=colnames
     )
-    p.iloc[
-        np.where(p['Graha'].isin([
+    # Replace 'Node' with Rahu & Ascendant with Lagna
+    p.loc[
+        p['Graha'].isin([
             'mean_Node', 'true_Node', 'Ascendant'
-        ]))[0], 
-        p.columns.get_indexer(['Graha'])
+        ]), 'Graha'
     ] = ['Rahu (mean)', 'Rahu (true)', 'Lagna']
-    p=add_ketu(p, 'true')
-    p=add_ketu(p, 'mean')
-    p.reset_index(drop=True)
+    # Add rows corresponding to Ketu
+    p=add_ketu(p)    
+    # Reorder rows sensibly
     p=reorder_swetest_rows(p)
     return p
 
@@ -69,30 +68,43 @@ def reorder_swetest_rows(p):
     p['ix']=[
         2, 3, 4, 5, 6, 7, 8, 11, 12, 13, 10, 9, 14, 15, 16, 17, 18, 
         19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 
-        35, 1, 36, 37, 38, 39, 40, 41, 42, 9.5, 10.5
+        35, 1, 36, 37, 38, 39, 40, 41, 42, 10.5, 9.5
     ]
     p.set_index('ix', inplace=True)    
     p=p.sort_index()
-    p=p.rename_axis(None)
+    p=p.reset_index(drop=True)
     return p
 
-def add_ketu(p, true_mean):
-    ketu_row=p[p['Graha']=='Rahu ('+true_mean+')']
-    ketu_row.loc[:, 'Graha']='Ketu ('+true_mean+')'
-    ketu_row.loc[:, 'House']=ketu_row.loc[:, 'House'] + 6
-    kdms=ketu_row.iloc[0].at['Lon°']
-    kms='°'+re.search(r'(?<=°).*', kdms).group(0)
-    kd=str((int(re.search(r'^\d+', kdms).group(0))+180)%360)
-    ketu_row.loc[:, 'Lon°']=kd+kms
-    ketu_row.loc[:, 'Lat°']=''
-    #ketu_row.at[0, 'Lon']=(ketu_row.at[0, 'Lon']+180)%360
-    p_out=pd.concat([p, ketu_row]) 
+def add_ketu(p):    
+    # Select rows corresponding to Rahu
+    ketu=p.loc[
+        p['Graha'].isin(['Rahu (true)', 'Rahu (mean)'])
+    ]
+    # Convenience function to add 180° to dms
+    def add_180_deg(x):
+        x_min_sec='°'+re.search(r'(?<=°).*', x).group(0)
+        x_deg=str((int(re.search(r'^\d+', x).group(0))+180)%360)
+        return x_deg+x_min_sec
+    # Replace 'Rahu' with 'Ketu' and add 180°
+    # House calculation not done here as can be done in one fell swoop
+    ketu.loc[:, ['Graha', 'Lon', 'Lon°']]=pd.DataFrame({
+        'Graha':ketu['Graha'].str.replace('Rahu', 'Ketu'), 
+        'Lon':ketu['Lon'].apply(lambda x: (x+180)%360), 
+        'Lon°':ketu['Lon°'].apply(add_180_deg)
+    })
+    p_out=pd.concat([p, ketu])
+    # Reset index required because concatenate creates repeated indices
+    p_out.reset_index(drop=True)
     return p_out
 
 def add_non_equi_col(p1, p2, p1col, p2col_high, p2col_low, p2col_get):
+    # Create column of p2's indices in p1 
+    # that correspond to value between high and low
     matched_idx=p1[p1col].apply(
         lambda x: p2[(p2[p2col_low]<=x) & (x<p2[p2col_high])].index.item()
     )
+    # Append columns from p2 corresponding to the indices
+    # These columns may contain repeated rows, so reset index
     p1[p2col_get]=p2.iloc[
         matched_idx, p2.columns.get_indexer(p2col_get)
     ].reset_index(drop=True)
