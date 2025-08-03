31c332fbf2b87fd688cb346d9dfef44845453d8f
Mangal <mangal@samay.com>
2025-08-03 20:07:06 +1200

    Add spaces around '=' in app.py

--- app.py
+++ app.py
@@ -18,56 +18,58 @@ dasa_nav_panel = ui.nav_panel(
     ui.output_data_frame(id = 'get_vimsottari_dasa')
 )
 
-moon_nav_panel=ui.nav_panel(
+moon_nav_panel = ui.nav_panel(
     'Lunar phase',
     #ui.output_data_frame(id='moon_phases')
     #output_widget(id='moon_phases')
 )
 
 app_ui = ui.page_fillable(
-    ui.output_ui(id='user_input'),
+    ui.output_ui(id = 'birth_input'),
     ui.navset_card_tab(
         ui.nav_control(ui.input_switch(
-            id='input_done', label='Show inputs', value=False
+            id = 'input_done', label = 'Show inputs', value = False
         )),
         ui.nav_spacer(),
         table_nav_panel,
         dasa_nav_panel,
         #moon_nav_panel,
         ui.nav_control(ui.input_dark_mode()),
-        id='pill'
+        id = 'pill'
     )
 )
 
 def server(input, output, session):
 
-    rvals=reactive.Value({'b_input_entered':False})
+    rvals = reactive.Value({'b_input_entered':False})
 
     @render.data_frame
     def filtered_places():
         # Filter file for only matches to entered birth place
-        cmd='grep -i "' + input.b_place() + '" places.txt'
+        cmd = 'grep -i "' + input.b_place() + '" places.txt'
         # Column names as reference. Not used as explained below
-        colnames=[
+        colnames = [
             'geonameid', 'name', 'asciiname', 'latitude', 'longitude', 
             'country code', 'elevation', 'population', 'timezone'
         ]
         # Providing column names makes it ignore column names present in file
-        places=std2pd.read_stdout(
-            cmd=cmd, reader='csv', sep='\t', col_names=colnames
+        places = std2pd.read_stdout(
+            cmd = cmd, reader = 'csv', sep = '\t', col_names = colnames
         )
-        places.sort_values(by=['population'], ascending=False, inplace=True)
-        return render.DataGrid(places, selection_mode='rows')
+        places.sort_values(
+            by = ['population'], ascending = False, inplace = True
+        )
+        return render.DataGrid(places, selection_mode = 'rows')
     
     @reactive.effect
     @reactive.event(input.search_place)
     def input_modal():
         # Modal containing places
-        m=ui.modal(
-            ui.output_data_frame(id='filtered_places'),
-            title='Click on a place to confirm details',
-            easy_close=True,
-            size='xl'
+        m = ui.modal(
+            ui.output_data_frame(id = 'filtered_places'),
+            title = 'Click on a place to confirm details',
+            easy_close = True,
+            size = 'xl'
         )
         ui.modal_show(m)
 
@@ -148,55 +150,55 @@ def server(input, output, session):
     #     return fig
 
     @render.ui
-    def user_input():
-        ui_out=ui.panel_conditional(
+    def birth_input():
+        ui_out = ui.panel_conditional(
             'input.input_done',
             ui.row(
                 ui.input_date(
-                    id='b_date',
-                    label='Input date',
-                    value=datetime.today().strftime('%Y-%m-%d'),
-                    format='yyyy-mm-dd',            
-                    weekstart=0,
-                    autoclose=True
+                    id = 'b_date',
+                    label = 'Input date',
+                    value = datetime.today().strftime('%Y-%m-%d'),
+                    format = 'yyyy-mm-dd',            
+                    weekstart = 0,
+                    autoclose = True
                 ), 
                 ui.input_text(
-                    id='b_time',
-                    label='Input time',
-                    value=datetime.now().strftime('%H:%M:%S')
+                    id = 'b_time',
+                    label = 'Input time',
+                    value = datetime.now().strftime('%H:%M:%S')
                 ),
                 ui.input_select(
-                    id='b_ayanamsa',
-                    label='Choose Ayanamsa',
-                    choices=ayanamsas.to_dict(), 
+                    id = 'b_ayanamsa',
+                    label = 'Choose Ayanamsa',
+                    choices = ayanamsas.to_dict(), 
                 ),
                 ui.input_text(
-                    id='b_place',
-                    label='Enter place',
-                    value='Auckland'
+                    id = 'b_place',
+                    label = 'Enter place',
+                    value = 'Auckland'
                 ),
                 ui.input_numeric(
-                    id='b_lon',
-                    label='Longitude',
-                    value=174.74304,
-                    min=-180,
-                    max=180
+                    id = 'b_lon',
+                    label = 'Longitude',
+                    value = 174.74304,
+                    min = -180,
+                    max = 180
                 ),
                 ui.input_numeric(
-                    id='b_lat',
-                    label='Latitude',
-                    value=-36.85582,
-                    min=-90,
-                    max=90
+                    id = 'b_lat',
+                    label = 'Latitude',
+                    value = -36.85582,
+                    min = -90,
+                    max = 90
                 ),
                 ui.input_text(
-                    id='b_tz',
-                    label='Timezone',
-                    value='Pacific/Auckland'
+                    id = 'b_tz',
+                    label = 'Timezone',
+                    value = 'Pacific/Auckland'
                 ),
                 ui.input_action_button(
-                    id='search_place',
-                    label='Search places'
+                    id = 'search_place',
+                    label = 'Search places'
                 )
             )
         )
