0909f44eb149b77438d2a6cd94bcbb98bfda151d
Samay <samay@samay.com>
2025-08-12 12:49:42 +1200

    Eliminate disk read-write operation by reading in stdout contents directly

--- stdout_to_pd.py
+++ stdout_to_pd.py
@@ -1,22 +1,22 @@
 import subprocess as sp
 import pandas as pd
 import os
+from io import StringIO
 
 def read_stdout(cmd, reader, sep, col_names):
-    tt = sp.run(cmd, stdout=sp.PIPE, shell=True, universal_newlines=True)
-    with open('out.txt', 'w') as output:
-        output.write(tt.stdout)
+    tt = sp.run(
+        args = cmd, capture_output = True, text = True, shell = True
+    )
     match reader:
         case 'table':
             p = pd.read_table(
-                filepath_or_buffer='out.txt', sep=sep, 
-                names = col_names, index_col=False   
+                filepath_or_buffer = StringIO(tt.stdout), sep = sep, 
+                names = col_names, index_col = False   
             )
         case 'csv':
             p = pd.read_csv(
-                filepath_or_buffer='out.txt', sep=sep, names=col_names, 
-                index_col=False             
+                filepath_or_buffer = StringIO(tt.stdout), sep = sep, 
+                names = col_names, index_col = False             
             )
-    os.remove('out.txt')
     return p
 
