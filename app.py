457d95ab4f80b2282f3f055836702000b7a51da4
Samay <samay@samay.com>
2025-08-24 23:19:03 +1200

    Qualify app ui elements to natal where applicable

--- app.py
+++ app.py
@@ -9,8 +9,8 @@ import sol_cross as sc
 
 natal_chart_ui = ui.accordion_panel(
     'Positions',    
-    ui.output_text(id = 'birth_info_chart'),
-    ui.output_data_frame(id = 'get_chart_data')    
+    ui.output_text(id = 'natal_info_chart'),
+    ui.output_data_frame(id = 'get_chart_data')
 )
 
 natal_dasa_ui = ui.accordion_panel(
@@ -30,9 +30,9 @@ natal_dasa_ui = ui.accordion_panel(
             }
         )
     ),
-    ui.output_text(id = 'birth_info_dasa'),
-    ui.output_text(id = 'offset_info_dasa'),
-    ui.output_data_frame(id = 'get_vimsottari_dasa')    
+    ui.output_text(id = 'natal_info_dasa'),
+    ui.output_text(id = 'natal_dasa_offset_info'),
+    ui.output_data_frame(id = 'natal_vimsottari_dasa_df')    
 )
 
 natal_ui = ui.nav_panel(
@@ -116,7 +116,7 @@ def server(input, output, session):
         ui.update_switch(id = 'sidebar', value = 'closed')
 
     @reactive.calc
-    def birth_chart():
+    def natal_chart():
         '''
         Return a chart object that can be reused across the app
         '''
@@ -138,7 +138,7 @@ def server(input, output, session):
 
     @render.data_frame
     def get_chart_data():
-        p = birth_chart().placements
+        p = natal_chart().placements
         # Round some cols
         p = mf.round_cols(p, ['Lon°', 'Speed'], [1, 3])
         # Keep subset
@@ -151,14 +151,14 @@ def server(input, output, session):
     @reactive.calc
     def natal_vimsottari_dasa():
         return vd.vimsottari_dasa(
-            chart = birth_chart(),
+            chart = natal_chart(),
             sub_dasa_level = int(input.vimsottari_dasa_sub_level()),
             dasa_offset_days = input.dasa_offset_days(),
             trunc_intervals = True
         )
 
     @render.data_frame
-    def get_vimsottari_dasa():
+    def natal_vimsottari_dasa_df():
         dasas = natal_vimsottari_dasa()
         # Currently, about 280px are used by other pills, radio buttons,
         # headers, etc. This value may need to be changed in the future if
@@ -169,12 +169,12 @@ def server(input, output, session):
         )
 
     @render.text
-    def birth_info_chart():
+    def natal_info_chart():
         # To give user feedback about birth place & time selection
-        return birth_chart().repr_str
+        return natal_chart().repr_str
 
     @render.text
-    def offset_info_dasa():
+    def natal_dasa_offset_info():
         dasa_offset_days = natal_vimsottari_dasa().dasa_offset_days
         if dasa_offset_days > 0:
             direction = 'future'
@@ -192,16 +192,16 @@ def server(input, output, session):
         return dasa_shift_text
 
     @render.text
-    def birth_info_dasa():
-        return birth_chart().repr_str
+    def natal_info_dasa():
+        return natal_chart().repr_str
 
     @reactive.calc
     def tajaka_chart():
         args = mf.chart_kwargs(
-            chart = birth_chart(),
+            chart = natal_chart(),
             dt = sc.sol_cross(
                 yr = int(input.tajaka_year()),
-                birth_crt = birth_chart(),
+                birth_crt = natal_chart(),
                 tropical = True
             )
         )
