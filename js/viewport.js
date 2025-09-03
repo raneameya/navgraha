87c8aab7597a6fd6f8678450ef37464859833641
Samay <samay@samay.com>
2025-09-03 22:00:05 +1200

    setInputValue is the preferred method

--- js/viewport.js
+++ js/viewport.js
@@ -3,12 +3,12 @@ var height = 0;
 $(document).on("shiny:connected", function() {
     width = window.innerWidth;
     height = window.innerHeight;
-    Shiny.onInputChange("width", width);
-    Shiny.onInputChange("height", height);
+    Shiny.setInputValue("width", width);
+    Shiny.setInputValue("height", height);
 });
 $(window).resize(function() {
     width = window.innerWidth;
     height = window.innerHeight;
-    Shiny.onInputChange("width", width);
-    Shiny.onInputChange("height", height);
+    Shiny.setInputValue("width", width);
+    Shiny.setInputValue("height", height);
 });
