509d3d698da43619c7809637a3e5f507b3e64730
Mangal <mangal@samay.com>
2026-02-19 20:01:09 +1300

    Add divisional_helper functions

---
+++ divisionals/divisional_helpers.py
@@ -0,0 +1,5 @@
+def add_house(p):
+    p['Sign'] = p['Lon'].apply(lambda x: int(divmod(x, 30)[0]+1))
+    lagna_rashi = p.loc[p['Graha']=='Lagna', 'Sign']
+    p['Bhava'] = p['Sign'].apply(lambda x: ((x+(12-lagna_rashi))%12)+1)
+    return p
