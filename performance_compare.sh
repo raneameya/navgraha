3eca435567b5f771e018ea446fbd208a921eec16
Samay <samay@samay.com>
2026-03-13 17:38:35 +1300

    Now outdated

--- performance_compare.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-source /mnt/chart_now/bin/activate.fish
-echo "Old method of individual columns of left and right intervals:"
-time python 'Performance non-equi old.py'
-echo "New method using fractional_interval class:"
-time python 'Performance non-equi new.py'
