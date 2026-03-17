0768d8a45a1660ec6de53cd6f44c8c7e41918a56
Mangal <mangal@samay.com>
2026-03-17 19:30:20 +1300

    File rename

--- core/data/constants.py
+++ core/data/constants.py
@@ -15,7 +15,7 @@ ayanamsas = [
     'Fagan/Bradley', 'Lahiri', 'De Luce', 'Raman', 'Usha/Shashi', 
     'Krishnamurti', 'Djwhal Khul', 'Yukteshwar', 'J.N. Bhasin', 
     'Babylonian/Kugler 1', 'Babylonian/Kugler 2', 'Babylonian/Kugler 3', 
-    'Babylonian/Huber',     'Babylonian/Eta Piscium', 
+    'Babylonian/Huber', 'Babylonian/Eta Piscium', 
     'Babylonian/Aldebaran = 15 Tau', 'Hipparchos', 'Sassanian', 
     'Galact. Center = 0 Sag', 'J2000', 'J1900', 'B1950', 'Suryasiddhanta', 
     'Suryasiddhanta (mean Sun)', 'Aryabhata', 'Aryabhata (mean Sun)', 
