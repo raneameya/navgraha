660c87351e09352261755cc1837645a4bcd37ee6
Samay <samay@samay.com>
2026-04-14 16:19:49 +1200

    Tajaka birthdate compute code more readable. It also now consistently uses yr_len. This unfortunately causes a +- 1 sec difference in calcs sometimes

--- core/tajaka/sol_cross.py
+++ core/tajaka/sol_cross.py
@@ -4,10 +4,10 @@ import core.chart.chart as crt
 from core.sweadaptor.swe_helper import get_sun_lon
 
 def sol_cross(
-    yr:int, 
-    birth_crt:crt.chart,
-    tropical:bool,
-    yr_len:float = 365.25
+    yr: int, 
+    birth_crt: crt.chart,
+    tropical: bool,
+    yr_len: float = 365.25
 ):
     # Estimated datetime at when longitides of Sun in the given year are 
     # equal to the longitudes of the Sun in the birth year
@@ -19,32 +19,27 @@ def sol_cross(
     )
     # Birth chart ayanamsa
     b_ay = birth_crt.swisseph_adaptor.ayanamsa
-    if tropical:
-        target_lon = get_sun_lon(dt = birth_crt.birth_event.dt, ay = 'Tropical')
-        init_lon = get_sun_lon(dt = init_datetime, ay = 'Tropical')
-    else:
-        target_lon = get_sun_lon(dt = birth_crt.birth_event.dt, ay = b_ay)
-        init_lon = get_sun_lon(dt = init_datetime, ay = b_ay)
+    # Chosen ayanamsa for tajaka computation
+    ay = 'Tropical' if tropical else b_ay
+    target_lon = get_sun_lon(dt = birth_crt.birth_event.dt, ay = ay)
+    init_lon = get_sun_lon(dt = init_datetime, ay = ay)
     # Account for targets being "behind" init lon as per 0 point (i.e. 0° Aries)
     if target_lon < init_lon:
         target_lon = target_lon + 360
     # Degrees/avg. solar speed in deg/day
-    estimated_days = (target_lon - init_lon - 3) / (360/365.2425)
+    estimated_days = (target_lon - init_lon - 3) / (360/yr_len)
     td = dt.timedelta(days = estimated_days)
-    loop_dt = init_datetime + td
+    estimate_dt = init_datetime + td
     deg_delta = 10000
     loop_num = 0
-    loop_lon = 0
+    estimate_lon = 0
     while abs(deg_delta) > 0.000001:
-        if tropical:
-            loop_lon = get_sun_lon(dt = loop_dt, ay = 'Tropical')
-        else:
-            loop_lon = get_sun_lon(dt = loop_dt, ay = b_ay)
-        deg_delta = (target_lon%360) - loop_lon
-        loop_td = dt.timedelta(days = deg_delta/(360/365))
-        loop_dt = loop_dt + loop_td
+        estimate_lon = get_sun_lon(dt = estimate_dt, ay = ay)
+        deg_delta = (target_lon % 360) - estimate_lon
+        loop_td = dt.timedelta(days = deg_delta / (360 / yr_len))
+        estimate_dt = estimate_dt + loop_td
         loop_num = loop_num + 1
         # Generally good enough estimate 10 loops in
         if loop_num > 10:
             break
-    return loop_dt
+    return estimate_dt
