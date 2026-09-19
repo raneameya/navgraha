46590cd3fa0760eff1117fcf7300c0b485007c26
Samay <samay@samay.com>
2026-09-19 23:43:05 +1200

    Create a non-empty default birth database to keep adding to

---
+++ core/data/create_birth_database.py
@@ -0,0 +1,39 @@
+import sqlite3
+
+connection = sqlite3.connect('./core/data/birth_data.db')
+
+cursor = connection.cursor()
+
+# Create birth data database
+cursor.execute('''
+CREATE TABLE IF NOT EXISTS births (
+    id INTEGER PRIMARY KEY,
+    name TEXT,
+    datetime TEXT,
+    latitude REAL,
+    longitude REAL,
+    timezone TEXT,
+    place TEXT
+)
+''')
+
+# Define seed data
+seed_data = [
+    (1, 'Anandmayi Maa', '1896-05-01 03:45:00', 22.780757, 91.083451, 'Asia/Dhaka', 'Kheore'),
+    (2, 'Jiddu Krishnamurti', '1895-05-12 00:30:00', 13.5668, 78.47611, 'Asia/Kolkata', 'Madanapalle'),
+    (3, 'Ramana Maharishi', '1879-12-30 01:00:00', 9.61228, 78.32321, 'Asia/Kolkata', 'Tiruchuli'),
+    (4, 'Osho', '1931-12-11 17:13:00', 23.144975740327727, 78.34920336707333, 'Asia/Kolkata', 'Kuchwada')
+]
+
+# Insert rows
+cursor.executemany("""
+INSERT OR REPLACE INTO births (id, name, datetime, latitude, longitude, timezone, place)
+VALUES (?, ?, ?, ?, ?, ?, ?)
+""", seed_data)
+
+# Test print
+cursor.execute("SELECT * FROM births")
+rows = cursor.fetchall()
+print(rows)
+
+connection.close()
